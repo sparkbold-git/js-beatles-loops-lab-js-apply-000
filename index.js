3146c22715b885f12d44c5ec51e7ce3669137659
Trung Hoang <sparkboldstudio@gmail.com>
2018-08-16 14:34:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // add solution here
+
+function theBeatlesPlay(musicians,instruments) {
+  for (var i = 0; i < musicians.length; i++) {
+    var array[i] = (musicians[i] + " plays " + instruments[i]);
+  }
+  return array;
+}
