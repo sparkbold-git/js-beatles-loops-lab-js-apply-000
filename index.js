c610531d6e5e84edd2e6edd9352ecb2cf6ae561e
Trung Hoang <sparkboldstudio@gmail.com>
2018-08-16 14:39:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,4 +8,17 @@ function theBeatlesPlay(musicians,instruments) {
   return array;
 }
 
-const facts = 
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
+function johnLennonFacts(facts) {
+  var array = [];
+  for (var i = 0; i < facts.length; i++) {
+    array.push(facts[i] + "!!!");
+  }
+  return array;
+}
